762c938b3b6e894b326506052b65c85c076728f8
r4start <r4start@local.host>
2012-06-20 15:16:34 +0400

    Fix micorosft RTTI mangle issue.

--- lib/AST/MicrosoftMangle.cpp
+++ lib/AST/MicrosoftMangle.cpp
@@ -410,7 +410,7 @@ void MicrosoftCXXNameMangler::mangleCXXRTTITypeDescriptor(
   
   // This is always reference?
   Out << "?A";
-  //mangleType(RD->getASTContext().getRecordType(RD)); // DAEMON! Todo!
+  mangleType(RD->getASTContext().getRecordType(RD), SourceRange());
   // End magic number
   Out << "@8";
 }
@@ -1990,7 +1990,7 @@ MicrosoftMangleContext::mangleCXXRTTITypeDescriptor(const CXXRecordDecl *RD,
 void MicrosoftMangleContext::mangleCXXRTTIName(QualType T,
                                                raw_ostream &Out) {
   MicrosoftCXXNameMangler mangler(*this, Out);
-  //mangler.mangleType(T); //DAEMON! Todo!
+  mangler.mangleType(T, SourceRange());
 }
 
 MSMangleContextExtensions* MicrosoftMangleContext::getMsExtensions() {
