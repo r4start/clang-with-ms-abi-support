7d9c63f0d5a700d48917abd4c5cd5c9868b80bf5
r4start <r4start@local.host>
2012-07-28 14:21:16 +0400

    1.Сделал, чтобы счетчик стейтов уменьшался при вызове деструкторов. 2. Сделал генерацию __ehhandler.

--- lib/CodeGen/CodeGenFunction.cpp
+++ lib/CodeGen/CodeGenFunction.cpp
@@ -44,7 +44,7 @@ CodeGenFunction::CodeGenFunction(CodeGenModule &cgm, bool suppressNewContext)
     IndirectBranch(0), SwitchInsn(0), CaseRangeBlock(0), UnreachableBlock(0),
     CXXABIThisDecl(0), CXXABIThisValue(0), CXXThisValue(0), CXXVTTDecl(0),
     CXXVTTValue(0), OutermostConditional(0), TerminateLandingPad(0),
-    TerminateHandler(0), TrapBB(0), MSTryState(0), IsMSABI(false) {
+    TerminateHandler(0), TrapBB(0), EHState(*this), IsMSABI(false) {
   if (!suppressNewContext)
     CGM.getCXXABI().getMangleContext().startNewFunction();
 
