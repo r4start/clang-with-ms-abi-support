b84d15a764670fbd966d4b24578142f87545a777
r4start <r4start@local.host>
2012-09-05 17:19:53 +0400

    Добавил новый тест и поправил генерацию unwindtable, чтобы она работала как и cl.

--- lib/CodeGen/CGException.cpp
+++ lib/CodeGen/CGException.cpp
@@ -629,10 +629,7 @@ void CodeGenFunction::EnterCXXTryStmt(const CXXTryStmt &S, bool IsFnTryBlock) {
       EHState.UnwindTable.back().IsUsed = true;
     }
 
-    if (!EHState.TryLevel) {
-      ++EHState.TopLevelTryNumber;
-    }
-
+    ++EHState.TopLevelTryNumber;
     ++EHState.TryLevel;
 
     size_t state = EHState.UnwindTable.size() - 1;
