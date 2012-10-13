6407852ef0e1f31267dd7d8464421def11f544bf
r4start <r4start@local.host>
2012-10-13 12:10:22 +0400

    Added new test case & description.

--- test/CodeGenCXX/ms-ctor-call.cpp
+++ test/CodeGenCXX/ms-ctor-call.cpp
@@ -1,5 +1,8 @@
 // RUN: %clang_cc1 -fms-extensions -fblocks -emit-llvm %s -o - -cxx-abi microsoft -triple=i386-pc-win32 | FileCheck %s
 
+// In this test we check passing hidden parameter to constructor.
+// This hidden parameter introduced by cl when class has virtual bases.
+
 namespace test {
 
 class A {
@@ -15,10 +18,16 @@ public:
   C(){}
 };
 
+class D : public C {
+public:
+  D(){}
+};
+
 void test1() {
   A a;
   B b;
   C c;
+  D d;
 }
 
 }
@@ -26,5 +35,8 @@ void test1() {
 // CHECK:      %call = call x86_thiscallcc %"class.test::A"* @"\01??0A@test@@QAE@XZ"(%"class.test::A"* %a) nounwind
 // CHECK-NEXT: %call1 = call x86_thiscallcc %"class.test::B"* @"\01??0B@test@@QAE@XZ"(%"class.test::B"* %b, i32 zeroext 1) nounwind
 // CHECK-NEXT: %call2 = call x86_thiscallcc %"class.test::C"* @"\01??0C@test@@QAE@XZ"(%"class.test::C"* %c, i32 zeroext 1)
+// CHECK-NEXT: %call3 = call x86_thiscallcc %class.test::class.D* @"\01??0D@@QAE@XZ"(%class.test::D* %d, i32 zeroext 1)
 
 // CHECK:      %call4 = call x86_thiscallcc %"class.test::B"* @"\01??0B@test@@QAE@XZ"(%"class.test::B"* %7, i32 zeroext 0) nounwind
+
+// CHECK:      %call5 = call x86_thiscallcc %class.test::class.C* @"\01??0C@@QAE@XZ"(%class.test::C* %8, i32 zeroext 0)
